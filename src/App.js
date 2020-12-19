32fe73bc1e59cd7745b2df249a970b2ccceba83b
miyoung <noonchicat@gmail.com>
2020-12-19 23:23:37 +0900

    button 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react';
 import 'App.css';
+import { Button } from 'antd';
+
+class Counter1 extends React.Component {
+  state = {
+    value: this.props.initialValue,
+  }
+
+  onClick = () => {
+    const { value } = this.state;
+    this.setState({ value: value + 1 });
+  };
+
+  render() {
+    const { value } = this.state;
+    return (
+      <div>
+        Counter1: {value}
+        <Button onClick={this.onClick}>+1</Button>
+      </div>
+    );
+  }
+}
+
+
 
 function App() {
   return (
     <div>
-      Hello, React!
+      <Counter1 initialValue={10}/>
+      <Counter1 initialValue={10}/>
+      <Counter1 initialValue={10}/>
     </div>
+
   );
 }
 
